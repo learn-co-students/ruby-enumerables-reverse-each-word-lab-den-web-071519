264ff7091cf8c18c5544ea43f667249144686ba2
Rob Ballantyne <raballantyne@gmail.com>
2019-07-11 20:19:39 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,11 @@
+
 def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
+
+#def reverse_each_word(sentence)
+#  sentence = sentence.split(' ')
+#  reversed = []
+#  sentence.each { |word| reversed.unshift(word) }
+#  return reversed.join(' ').reverse
+#end
